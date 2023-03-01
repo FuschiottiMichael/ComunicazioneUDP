3893ee9197c6c0d5a311930b2f74583cb523fa35
Fuschiotti Michael <fuschiottimichael68gmail.com>
2023-03-01 19:21:37 +0100

    AGGIORNAMENTO, quasi completo

--- src/comunicazioneudp/UDPClient.java
+++ src/comunicazioneudp/UDPClient.java
@@ -4,6 +4,17 @@
  */
 package comunicazioneudp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author michaelfuschiotti
@@ -15,6 +26,57 @@ public class UDPClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            int portaServer = 2000;
+            InetAddress IPServer = InetAddress.getByName("localhost");
+            
+            byte[] bufferOUT = new byte[1024];
+            byte[] bufferIN = new byte[1024];
+         
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            
+            try {
+                //creo un socket e faccio inserire un dato all'utente
+                DatagramSocket clientSocket = new DatagramSocket();
+                System.out.println("Client pronto - inserisci un dato da inviare:");
+                
+                String daSpedire = input.readLine(); //leggo la stringa inserita dall'utente e successivamente predispongo il buffer di uscita
+                bufferOUT = daSpedire.getBytes();
+                
+                //creo il datagram packet con il messaggio da inviare e i parametri del server
+                DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPServer, portaServer);
+                clientSocket.send(sendPacket);
+                
+                DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+                clientSocket.receive(receivePacket);
+                String ricevuto = new String(receivePacket.getData());
+                
+                int numCaratteri = receivePacket.getLength();
+                ricevuto = ricevuto.substring(0, numCaratteri);
+                System.out.println("dal SERVER:" +ricevuto);
+                
+                clientSocket.close();
+                
+            } catch (SocketException ex) {
+                Logger.getLogger(UDPClient.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println("Errore nel socket!");
+            }
+            catch (IOException ex) {
+                Logger.getLogger(UDPClient.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println("Errore di I/O!");
+            }
+            
+   
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(UDPClient.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Errore DNS!");
+        }
+        
+        
+        
+        
+        
+        
     }
     
 }
--- src/comunicazioneudp/UDPServer.java
+++ src/comunicazioneudp/UDPServer.java
@@ -24,40 +24,51 @@ public class UDPServer {
     public static void main(String[] args) {
         try {
             DatagramSocket serverSocket = new DatagramSocket(2000);
-            boolean attivo = true;
+            boolean attivo = true; //utilizzo questa variabile come condizione di ripetizione/uscita dal ciclo
             byte[] bufferIN = new byte[1024]; //definisco i due buffer per i dati con dimensione di 1024
             byte[] bufferOUT = new byte[1024];
             
             System.out.println("SERVER AVVIATO...");
             while(attivo){
-                DatagramPacket receivePacket = new DatagramPacket(bufferIN,bufferIN.length); //definisco il datagramma
+                DatagramPacket receivePacket = new DatagramPacket(bufferIN,bufferIN.length); //definisco il datagramma e successivamente mi metto in attesa di ricezione dei dati da parte del client 
                 try {
                     serverSocket.receive(receivePacket); //attesa della ricezione, messaggio del client
                 } catch (IOException ex) {
                     Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+                    System.err.println("Errore di I/O!");
                 }
-                String ricevuto = new String(receivePacket.getData());
-                int numeroCaratteri = receivePacket.getLength();
-                ricevuto = ricevuto.substring(0,numeroCaratteri);
+                String ricevuto = new String(receivePacket.getData()); //analizzo il pacchetto ed estraggo il messaggio. IL PACCHETTO ARRIVA IN ARRAY DI BYTE, CHE LI CONVERTO IN UNA STRINGA
+                int numeroCaratteri = receivePacket.getLength(); //individuo la lunghezza impostata alla trasmissione
+                ricevuto = ricevuto.substring(0, numeroCaratteri); //elimino i caratteri superflui presenti nel buffer 
                 System.out.println("RICEVUTO: "+ricevuto);
                 
-                InetAddress IPClient = receivePacket.getAddress();
-                int portaClient = receivePacket.getPort();
+                InetAddress IPClient = receivePacket.getAddress(); //sempre dal pacchetto ricevuto, individuo i parametri per la trasmissione della risposta, QUA INDIVIDUO L'INDIRIZZO
+                int portaClient = receivePacket.getPort(); //INDIVIDUO LA PORTA 
                 
-                String messDaSpedire = ricevuto.toUpperCase();
-                bufferOUT = messDaSpedire.getBytes();
+                String messDaSpedire = ricevuto.toUpperCase(); //preparo la risposta, trasformando il messaggio ricevuto in maiuscolo
+                bufferOUT = messDaSpedire.getBytes(); //Converto la risposta da String a byte e successivamente vado a creare un datagramma per l'uscita del pacchetto 
                 
                 DatagramPacket sendPacket = new DatagramPacket(bufferOUT,bufferOUT.length, IPClient, portaClient);
                 try {
                     serverSocket.send(sendPacket);
                 } catch (IOException ex) {
                     Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+                    System.err.println("Errore di I/O!");
                 }
                 
+                
+                if(ricevuto.equals("fine"));
+                {
+                    System.out.println("SERVER IN CHIUSURA... Buona giornata!!!");
+                    attivo = false;
+                }
+                
+                
             }
-     
+         serverSocket.close();
         } catch (SocketException ex) {
             Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Errore nel socket!");
         }
         
     }
